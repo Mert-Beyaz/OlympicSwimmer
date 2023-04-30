266bd006a2e18e7fe630c5ed1d68d9052045295b
erdemturk <mertbeyaz@posta.mu.edu.tr>
2023-04-30 12:12:45 +0300

    Exit Button and try again button

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -1,13 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonController : MonoBehaviour
 {
     public PlayerController player;
- public void StartSwimming()
+    public void StartSwimming()
     {
         player.isPlayerJump = true;
         this.gameObject.SetActive(false);
     }
+    public void OnTryAgainClick()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
 }
+
